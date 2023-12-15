6fefdbfc7a52d22c6c955834eec6049e1ed6c5ff
Haymant Lee <dev@lizhao.net>
2023-12-15 11:31:39 +0800

    added jsonforms

--- src/components/FormBuilder.tsx
+++ src/components/FormBuilder.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Setting from '../schema/Setting.json';
+import defaultui from '../schema/defaultui.json';
+
+import {
+  materialRenderers,
+  materialCells,
+} from '@jsonforms/material-renderers';
+import { JsonForms } from "@jsonforms/react";
 
 const FormBuilder: React.FC<any> = () => {
+  const [data, setData] = useState({});
   return (
-    <div>dd</div>
+    <div className='App'>
+      <JsonForms
+        schema={Setting}
+        data={data}
+        renderers={materialRenderers}
+        cells={materialCells}
+        onChange={({ data, errors }) => setData(data)}
+      />
+    </div>
   );
 };
 
